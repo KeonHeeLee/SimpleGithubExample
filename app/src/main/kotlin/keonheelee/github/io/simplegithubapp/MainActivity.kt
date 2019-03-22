c9579e1be0c740211efdc3ebedc25f7b5e8eb1c4
KeonHeeLee <beta1360@naver.com>
2019-03-22 20:45:09 +0900

    (+) init#02 : project

---
+++ app/src/main/kotlin/keonheelee/github/io/simplegithubapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package keonheelee.github.io.simplegithubapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
